a4536bbcc90a4fec850038ca25e020909243eb25
Hemanth kumar <phemanthkumar509@gmail.com>
2026-04-02 06:02:31 +0530

    hg

--- server/utils/db.js
+++ server/utils/db.js
@@ -4,7 +4,7 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const DATA_DIR = path.join(__dirname, '../data');
+const DATA_DIR = path.join(__dirname, '../../data');
 
 // Helper to ensure data directory exists
 const ensureDataDir = async () => {
